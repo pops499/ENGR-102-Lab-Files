13269e5b5c1bfc25a7e32516f2184eacef65ffd3
Daniel Garcia <82914199+pops499@users.noreply.github.com>
2024-08-30 15:26:13 -0500

    Add files via upload

---
+++ follow_directions.py
@@ -0,0 +1,45 @@
+
+import math
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Daniel Garcia
+# Section:      544
+# Assignment:   1.13 LAB: Follow directions
+# Date:         22 August 2024
+#
+
+print ("This shows the evaluation of (1-cos(x))/x^2 evaluated close to x=0")
+print ("My guess is 0.1")
+print ()
+x = 1.0
+function = (1 - math.cos(x)) / math.pow(x , 2)
+print (function)
+x = 0.1
+function = (1 - math.cos(x)) / math.pow(x , 2)
+print (function)
+x = 0.01
+function = (1 - math.cos(x)) / math.pow(x , 2)
+print (function)
+x = 0.001
+function = (1 - math.cos(x)) / math.pow(x , 2)
+print (function)
+x = 0.0001
+function = (1 - math.cos(x)) / math.pow(x , 2)
+print (function)
+x = 0.00001
+function = (1 - math.cos(x)) / math.pow(x , 2)
+print (function)
+x = 0.000001
+function = (1 - math.cos(x)) / math.pow(x , 2)
+print (function)
+x = 0.0000001
+function = (1 - math.cos(x)) / math.pow(x , 2)
+print (function)
+
+print()
+print("my guess was off") #comment
+
+
+
