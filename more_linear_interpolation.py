4f93c826e929bca44f23211cbe8b09c91675692b
Daniel Garcia <82914199+pops499@users.noreply.github.com>
2024-08-30 14:49:11 -0500

    Add files via upload

---
+++ more_linear_interpolation.py
@@ -0,0 +1,176 @@
+import math
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Daniel Garcia
+# Section:      544
+# Assignment:   2.10 LAB: More Linear Interpolation
+# Date:         22 August 2024
+#
+
+# time for x at 30 seconds
+x1 = 12
+x2 = 85
+y1 = 8
+y2 = -5
+slope = ((y2 - y1) / (x2 - x1))
+x = 30.0
+linear_interpolation = y1 + (slope) * (x - x1)
+print("At time 30.0 seconds:")
+print("x1 =", linear_interpolation, "m" )
+
+# time for y at 30 seconds
+x1 = 12
+x2 = 85
+y1 = 6
+y2 = 30
+slope = ((y2 - y1) / (x2 - x1))
+x = 30.0
+linear_interpolation = y1 + (slope) * (x - x1)
+print("y1 =", linear_interpolation, "m" )
+
+# time for z at 30 seconds
+x1 = 12
+x2 = 85
+y1 = 7
+y2 = 9
+slope = ((y2 - y1) / (x2 - x1))
+x = 30.0
+linear_interpolation = y1 + (slope) * (x - x1)
+print("z1 =", linear_interpolation, "m" )
+
+print ("-----------------------")
+
+# time for x at 37.5 seconds
+x1 = 12
+x2 = 85
+y1 = 8
+y2 = -5
+slope = ((y2 - y1) / (x2 - x1))
+x = 37.5
+linear_interpolation = y1 + (slope) * (x - x1)
+print("At time 37.5 seconds:")
+print("x2 =", linear_interpolation, "m" )
+
+# time for y at 37.5
+x1 = 12
+x2 = 85
+y1 = 6
+y2 = 30
+slope = ((y2 - y1) / (x2 - x1))
+x = 37.5
+linear_interpolation = y1 + (slope) * (x - x1)
+print("y2 =", linear_interpolation, "m" )
+
+# time for z at 37.5
+x1 = 12
+x2 = 85
+y1 = 7
+y2 = 9
+slope = ((y2 - y1) / (x2 - x1))
+x = 37.5
+linear_interpolation = y1 + (slope) * (x - x1)
+print("z2 =", linear_interpolation, "m" )
+
+print ("-----------------------")
+
+# time for x at 37.5 seconds
+x1 = 12
+x2 = 85
+y1 = 8
+y2 = -5
+slope = ((y2 - y1) / (x2 - x1))
+x = 45.0
+linear_interpolation = y1 + (slope) * (x - x1)
+print("At time 45.0 seconds:")
+print("x3 =", linear_interpolation, "m" )
+
+# time for y at 37.5
+x1 = 12
+x2 = 85
+y1 = 6
+y2 = 30
+slope = ((y2 - y1) / (x2 - x1))
+x = 45.0
+linear_interpolation = y1 + (slope) * (x - x1)
+print("y3 =", linear_interpolation, "m" )
+
+# time for z at 37.5
+x1 = 12
+x2 = 85
+y1 = 7
+y2 = 9
+slope = ((y2 - y1) / (x2 - x1))
+x = 45.0
+linear_interpolation = y1 + (slope) * (x - x1)
+print("z3 =", linear_interpolation, "m" )
+
+print ("-----------------------")
+
+# time for x at 37.5 seconds
+x1 = 12
+x2 = 85
+y1 = 8
+y2 = -5
+slope = ((y2 - y1) / (x2 - x1))
+x = 52.5
+linear_interpolation = y1 + (slope) * (x - x1)
+print("At time 52.5 seconds:")
+print("x4 =", linear_interpolation, "m" )
+
+# time for y at 37.5
+x1 = 12
+x2 = 85
+y1 = 6
+y2 = 30
+slope = ((y2 - y1) / (x2 - x1))
+x = 52.5
+linear_interpolation = y1 + (slope) * (x - x1)
+print("y4 =", linear_interpolation, "m" )
+
+# time for z at 37.5
+x1 = 12
+x2 = 85
+y1 = 7
+y2 = 9
+slope = ((y2 - y1) / (x2 - x1))
+x = 52.5
+linear_interpolation = y1 + (slope) * (x - x1)
+print("z4 =", linear_interpolation, "m" )
+
+print ("-----------------------")
+
+# time for x at 37.5 seconds
+x1 = 12
+x2 = 85
+y1 = 8
+y2 = -5
+slope = ((y2 - y1) / (x2 - x1))
+x = 60.0
+linear_interpolation = y1 + (slope) * (x - x1)
+print("At time 60.0 seconds:")
+print("x5 =", linear_interpolation, "m" )
+
+# time for y at 37.5
+x1 = 12
+x2 = 85
+y1 = 6
+y2 = 30
+slope = ((y2 - y1) / (x2 - x1))
+x = 60.0
+linear_interpolation = y1 + (slope) * (x - x1)
+print("y5 =", linear_interpolation, "m" )
+
+# time for z at 37.5
+x1 = 12
+x2 = 85
+y1 = 7
+y2 = 9
+slope = ((y2 - y1) / (x2 - x1))
+x = 60.0
+linear_interpolation = y1 + (slope) * (x - x1)
+print("z5 =", linear_interpolation, "m" )
+
+
+
