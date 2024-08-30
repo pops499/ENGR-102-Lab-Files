57be6bc1ee6ddd92213feb0d8fc0a752d3b8d691
Daniel Garcia <82914199+pops499@users.noreply.github.com>
2024-08-30 14:54:09 -0500

    Add files via upload

---
+++ python_code_soup.py
@@ -0,0 +1,191 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Daniel Garcia
+# Section:      544
+# Assignment:   2.10 LAB: More Linear Interpolation
+# Date:         22 August 2024
+#
+
+# for 1
+z = 0
+x = 1
+z += x
+print(z)
+
+# for 28
+y = 10
+x += 1
+y *= x
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+y += x
+z += y
+print (z)
+
+# for 102
+x = 1
+y = 10
+z = 0
+x = y
+y *= x
+z += y
+x = 1
+x += 1
+z += x
+print (z)
+
+# for 10000000000000
+
+x = 1
+y = 10
+z = 0
+x = y
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+z += y
+
+print (z)
+
+# for 8675
+x = 1
+y = 10
+z = 0
+x = y
+y *= x
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+
+
+y *= x
+z += y
+
+x = 1
+y = 10
+y *= x
+x += 1
+x += 1
+
+y *= x
+x = 1
+y = 10
+x += 1
+y *= x
+
+z += y
+
+
+y *= x
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+x += 1
+z += x
+
+
+z += y
+
+
+
+
+
+
+print (z)
+
+
